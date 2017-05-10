5f68c40b7ca8c46511ff5309a3a38641375d1ff1
Calvin <mrcalvinkc@gmail.com>
2017-05-09 21:32:04 -0700

    Added method classes for different ways to manipulate data display

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def full_name
+    @full_name = first + " " + last
+  end
+
+  def full_number
+    @full_number = "+81 " + number.to_s
+  end
+
 end
